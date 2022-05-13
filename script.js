1810a4fc7c2142bbc69d96c4a197c0a094d4b9bd
srilatha kasireddy <kasireddysrilatha17@gmail.com>
2022-05-13 16:19:06 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,13 @@
+document.getElementById("data").innerHTML="10";
+setTimeout(()=>document.getElementById("data").innerHTML="9",1000)
+setTimeout(()=>document.getElementById("data").innerHTML="8",2000)
+setTimeout(()=>document.getElementById("data").innerHTML="7",3000)
+setTimeout(()=>document.getElementById("data").innerHTML="6",4000)
+setTimeout(()=>document.getElementById("data").innerHTML="5",5000)
+setTimeout(()=>document.getElementById("data").innerHTML="4",6000)
+setTimeout(()=>document.getElementById("data").innerHTML="3",7000)
+setTimeout(()=>document.getElementById("data").innerHTML="2",8000)
+setTimeout(()=>document.getElementById("data").innerHTML="1",9000)
+setTimeout(()=>document.getElementById("data").innerHTML="Happy Independence Day!!!😊",10000)
+
+
